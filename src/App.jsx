220dbbbba27ed0960449ee2bafb0ef4f5bfcaf6d
perpetualofosu <ewuramapokuah.p.o@gmail.com>
2024-06-22 05:23:26 +0000

    Route added for signup page

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './Homepage'; 
 import LoginPage from './Login'; 
 import './App.css'; 
+import SignUpCard from './Signup';
 
 function App() {
   return (
     <Router>
       <Routes>
+      <Route path="/Signup" element={<SignUpCard/>} />
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
       </Routes>
