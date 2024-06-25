c99bfaec26d4707b34af56a7c097d7143442f7f3
perpetualofosu <ewuramapokuah.p.o@gmail.com>
2024-06-25 20:39:13 +0000

    Background images added to Middle section

--- src/Middle.jsx
+++ src/Middle.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import axios from './axios';
-import './middle.css'
+import './middle.css';
 
 const Row = ({ title, fetchUrl, bigRow = false }) => {
     const [movies, setMovies] = useState([]);
@@ -15,7 +15,7 @@ const Row = ({ title, fetchUrl, bigRow = false }) => {
                 setMovies(request.data.results || []);
             } catch (error) {
                 console.error("Error fetching data: ", error);
-                setMovies([]); // Ensure movies is an array even if the request fails
+                setMovies([]);
             }
         }
         fetchData();
@@ -23,29 +23,28 @@ const Row = ({ title, fetchUrl, bigRow = false }) => {
 
     return (
         <div className="row">
-            <h2>{title}</h2>
-            <div className='overflow-auto'>
-            <div className="row__posters">
-                {movies.map(movie => (
-                    (movie.poster_path || movie.backdrop_path) && (
-                        <div
-                            className={`row__poster ${bigRow ? "row__posterLarge" : ""}`}
-                            key={movie.id}
-                        >
-                            <img
-                                src={`${base_url}${bigRow ? movie.poster_path : movie.backdrop_path}`}
-                                alt={movie.name || movie.title}
-                            />
-                            <div className="row__overlay">
-                                <h3>{movie.title || movie.name}</h3>
-                                <p>{movie.overview}</p>
-                                <p>Rating: {movie.vote_average}</p>
+            <h2 className="row__title">{title}</h2>
+            <div className="movies">
+                <div className="row__posters">
+                    {movies.map(movie => (
+                        (movie.poster_path || movie.backdrop_path) && (
+                            <div
+                                className={`row__poster ${bigRow ? "row__posterLarge" : ""}`}
+                                key={movie.id}
+                            >
+                                <img
+                                    src={`${base_url}${bigRow ? movie.poster_path : movie.backdrop_path}`}
+                                    alt={movie.name || movie.title}
+                                />
+                                <div className="row__overlay">
+                                    <h3>{movie.title || movie.name}</h3>
+                                    <p>{movie.overview}</p>
+                                    <p>Rating: {movie.vote_average}</p>
+                                </div>
                             </div>
-                        </div>
-                        
-                    )
-                ))}
-            </div>
+                        )
+                    ))}
+                </div>
             </div>
         </div>
     );
