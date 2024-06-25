247e578346ffb21fc7c257d172ffe53e69dc1fba
perpetualofosu <ewuramapokuah.p.o@gmail.com>
2024-06-25 20:42:12 +0000

    Login page renamed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './Homepage'; 
-import LoginPage from './Login'; 
+import ProfilePage from './Profile'; 
 import './App.css'; 
 import SignUpCard from './Signup';
 import SearchResults from './SearchResults';
 import WelcomePage from './Welcome';
 import { CategoryProvider } from './CategoryContext';
+import Login from './LoginPage';
+
 
 function App() {
   return (
@@ -14,9 +16,10 @@ function App() {
     <Router>
       
       <Routes>
+      <Route path="/Login" element={<Login/>} />
       <Route path="/Signup" element={<SignUpCard/>} />
         <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
+        <Route path="/Profile" element={<ProfilePage />} />
         <Route path="/search" element={<SearchResults />} />
         <Route path='/welcome' element={<WelcomePage />} />
         
