4fa374b94ace5d63492f39fa9c3af553ad781f27
perpetualofosu <ewuramapokuah.p.o@gmail.com>
2024-07-10 10:07:55 +0000

    Language feature added

---
+++ src/LanguageContext.jsx
@@ -0,0 +1,20 @@
+import React, { createContext, useState, useContext } from 'react';
+
+const LanguageContext = createContext();
+
+export const LanguageProvider = ({ children }) => {
+  const [language, setLanguage] = useState('en');
+
+  const changeLanguage = (lang) => {
+    setLanguage(lang);
+    localStorage.setItem('language', lang);
+  };
+
+  return (
+    <LanguageContext.Provider value={{ language, changeLanguage }}>
+      {children}
+    </LanguageContext.Provider>
+  );
+};
+
+export const useLanguage = () => useContext(LanguageContext);
