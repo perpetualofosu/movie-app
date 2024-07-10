e07d55a7c3d82ba047b362d8bd5cad5e034fdcee
perpetualofosu <ewuramapokuah.p.o@gmail.com>
2024-07-10 05:01:13 +0000

    Welcome, verification routes added

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import './i18n';
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './Homepage'; 
@@ -8,25 +9,33 @@ import SearchResults from './SearchResults';
 import WelcomePage from './Welcome';
 import { CategoryProvider } from './CategoryContext';
 import Login from './LoginPage';
+import { AuthProvider } from './AuthContext';
+import VerificationPage from './VerificationPage';
+import ProfileSettings from './ProfileSettings';
+import { LanguageProvider } from './LanguageContext';
+import MovieDetails from './MovieDetails';
 
 
 function App() {
   return (
     <CategoryProvider>
-    <Router>
-      
-      <Routes>
-      <Route path="/Login" element={<Login/>} />
-      <Route path="/Signup" element={<SignUpCard/>} />
-        <Route path="/" element={<HomePage />} />
-        <Route path="/Profile" element={<ProfilePage />} />
-        <Route path="/search" element={<SearchResults />} />
-        <Route path='/welcome' element={<WelcomePage />} />
-        
-
-
-      </Routes>
-    </Router>
+      <AuthProvider>
+        <LanguageProvider>
+          <Router>
+            <Routes>
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<SignUpCard />} />
+              <Route path="/verify-email" element={<VerificationPage />} />
+              <Route path="/" element={<HomePage />} />
+              <Route path="/profile" element={<ProfilePage />} />
+              <Route path="/search" element={<SearchResults />} />
+              <Route path="/welcome" element={<WelcomePage />} />
+              <Route path="/profile-settings" element={<ProfileSettings />} />
+              <Route path="/movie/:id" element={<MovieDetails />} />
+            </Routes>
+          </Router>
+        </LanguageProvider>
+      </AuthProvider>
     </CategoryProvider>
   );
 }
